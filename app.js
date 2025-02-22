39648bdf7b5a17c07e27bdac37295f654cd33270
Julio Chagas <juliu12@outlook.com.br>
2025-02-22 16:29:38 -0300

    Criação do array e as funções adicionarAmigo e limparCampo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(nome){
+    let nome = document.querySelector('input').value;
+
+    if (nome == ''){
+        alert('Por favor, insira um nome!');
+    }else{
+        amigos.push(nome);
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    nome = document.querySelector('input');
+    nome.value = '';
+}
